0b511cde87fdc3e2430be127b21904d432a54ea1
shpeeck <67144527+shpeeck@users.noreply.github.com>
2021-11-04 17:36:54 +0100

    homework 1-2

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./css/main.css";
+import ShopItemFunc from "./components/Shopitem/ShopItemFunc";
+
+const item = {
+  brand: "Tiger of Sweden",
+  title: "Leonard coat",
+  description: "Minimalistic coat in cotton-blend",
+  descriptionFull:
+    "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+  price: 399,
+  currency: "£"
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <ShopItemFunc item={item} />
+    </div>
+  );
+}
